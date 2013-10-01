c977d7af53a2d16f3c6dd231250cfa421abb2118
Khalid <khalidsookia@gmail.com>
2013-10-01 22:17:26 +0100

    product entity description => nullable = false
    else form is not validated since tinymce does not send text to textarea before validating the form

--- src/App/ProductBundle/Entity/Product.php
+++ src/App/ProductBundle/Entity/Product.php
@@ -33,7 +33,7 @@ class Product
     /**
      * @var string
      *
-     * @ORM\Column(name="description", type="text")
+     * @ORM\Column(name="description", type="text", nullable=true)
      */
     private $description;
 
