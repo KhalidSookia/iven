629f5656bc6d45e31ffc592dab1038a9bf9cc295
Khalid <khalidsookia@gmail.com>
2013-10-02 22:48:33 +0100

    Removal of codeBundle
    I removed codebundle because i will add an option for the admin to send a code directly using an email.
    thus there will be a code issued on the email entered by the admin and i need to figure out how to manage all this.
    also i need to see what to do for the authorisations and the roles
    Need to have a better look on SDZ tuto for the userBundle without fosuserbundle
    I need also to look at firewalls and security params

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new App\AppBundle\AppAppBundle(),
-            new App\CodeBundle\AppCodeBundle(),
             new App\MenuBundle\AppMenuBundle(),
             new App\ProductBundle\AppProductBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
--- src/App/CodeBundle/AppCodeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\CodeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AppCodeBundle extends Bundle
-{
-}
